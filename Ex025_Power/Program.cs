c2f058551a8d0746c687dfb0ed16f4949c62ede2
Andrey Khudyakov <avh_2004@mail.ru>
2022-10-25 01:18:13 +0500

    Added Exersize №25 Power

---
+++ Ex025_Power/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и 
+// возводит число A в натуральную степень B. Применять Math.Pow нельзя!
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+double GetBasis(string msg)
+{
+    Console.WriteLine(msg);
+    double a = double.Parse(Console.ReadLine());
+    return a;
+}
+
+int GetPower(string msg)
+{
+    Console.WriteLine(msg);
+    int b = int.Parse(Console.ReadLine());
+    return b;
+}
+
+bool ValidPower(int b)
+{
+    if (b < 0)
+    {
+        Console.WriteLine("Неверное значение показателя степени.");
+        return false;
+    }
+    return true;
+}
+
+double CalculatePower(double a, int b)
+{
+    double result = a;
+    if (b == 0)
+    {
+        result = 1;
+        return result;
+    }
+    for (int i = 2; i <= b; i++)
+    {
+        result *= a;
+    }
+    return result;
+}
+
+double A = GetBasis("Введите значение основания степени:");
+int B = GetPower("Введите натуральное значение показателя степени:");
+if (ValidPower(B)) Console.WriteLine($"{A} в степени {B} равно {CalculatePower(A, B)}");
