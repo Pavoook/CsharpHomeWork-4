1c9fc5a8284bc201c671cac38d78c51913e435c5
Andrey Khudyakov <avh_2004@mail.ru>
2022-10-25 01:41:36 +0500

    Added Exersize №27 Sum Of Digits

---
+++ Ex027_SumOfDigits/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и 
+// выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int GetNumber(string msg)
+{
+    Console.WriteLine(msg);
+    int number = Math.Abs(int.Parse(Console.ReadLine()));
+    return number;
+}
+
+int CalculateSum(int value)
+{
+    int sum = 0;
+    while (value != 0)
+    {
+        sum += value % 10;
+        value /= 10;
+    }
+    return sum;
+}
+
+int Number = GetNumber("Введите целое число:");
+Console.WriteLine("Сумма цифр равна " + CalculateSum(Number));
